d292b0f2fe412ae526e84ce17baa5c749841835c
Vangie Du <duwan@live.com>
2025-03-30 11:35:59 +0800

    :arrow_up: deps: update Gbox to version 0.0.5 with new SHA256

--- gbox.rb
+++ gbox.rb
@@ -5,9 +5,9 @@
 class Gbox < Formula
   desc "Self-hostable sandbox for MCP and AI agents"
   homepage "https://github.com/babelcloud/gru-sandbox"
-  version "0.0.4"
+  version "0.0.5"
   url "https://github.com/babelcloud/gru-sandbox/releases/download/v#{version}/gbox-#{version}.tar.gz"
-  sha256 "0da7bee6424150dc57ebdb4d186bf8289ab38d1d257430b30c5e54219e4db3dd"
+  sha256 "6e2583056207f51af764a861d6a9f078b31db5246b1f2aae368a25158a8133c3"
 
   depends_on "carvel-dev/carvel/ytt"
   depends_on "carvel-dev/carvel/kapp"
